e21db3fc1d0f2a5545a3695973d5acdb80aed505
baigall <2045973038@qq.com>
2019-06-10 15:41:06 +0800

    访问规则
    派生类访问 基类的访问规则

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+class CBase 
+{
+protected:
+	int a;
+public:
+	CBase();
+	CBase(int c);
+	void display();
+};
+CBase::CBase()
+{
+	a = 0;
+}
+CBase::CBase(int c)
+{
+	a = c;
+}
+void CBase::display()
+{
+	cout << "a = " << a << endl;
+}
+class CDerived :public CBase
+{
+public:
+	CDerived()
+	{
+		a = 0;
+	}
+	CDerived(int c)
+	{
+		a = c;
+	}
+};
+int main()
+{
+	CDerived od(202);
+	od.display();
+    return 0;
+}
+
